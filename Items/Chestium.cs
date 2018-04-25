436d44170cf6d89a74b872bdc3dc20a1a8dfe203
whitmakr <35874812+whitmakr@users.noreply.github.com>
2018-04-25 08:56:49 -0500

    Chest and Material CS files

---
+++ Items/Chestium.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace MagicStorage.Items
+{
+    public class Chestium : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Chestium");
+            Tooltip.SetDefault("A material specifically used to make the Magic Chest.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.material = true;
+            item.maxStack = 99;
+            item.width = 16;
+            item.height = 16;
+            item.UseSound = SoundID.Item2;
+            item.value = Item.buyPrice(0, 5, 0, 0);
+            item.rare = 5;
+        }
+    }
+}
---
+++ Items/MagicChest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace MagicStorage.Items
+{
+    public class MagicChest : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Magic Chest");
+            Tooltip.SetDefault("A user-friendly magical chest that makes storing and finding items that are stored much easier.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 26;
+            item.height = 22;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.rare = 5;
+            item.value = Item.buyPrice(0, 6, 0, 0);
+            item.createTile = mod.TileType("MagicChest");
+            item.placeStyle = 1;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Chest);
+            recipe.AddIngredient(mod.ItemType("Chestium"));
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
