ec1b42f58f54a3354fde33fd19887e8fefc0d67c
Dylan Ascencio <dylanascen@gmail.com>
2018-04-30 09:15:00 -0500

    Added resources for the Decorative Chest item.

---
+++ Tiles/UselessChest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using System.Collections.Generic;
+using Terraria.World.Generation;
+using Terraria.GameContent.Generation;
+
+namespace MagicStorage.Tiles
+{
+    class UselessChest : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Decorative Chest");
+            AddMapEntry(new Color(144, 148, 144), name);
+            dustType = 11;
+            disableSmartCursor = true;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 48, mod.ItemType<Items.UselessChest>());
+        }
+    }
+}
